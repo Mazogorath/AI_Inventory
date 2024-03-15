a234d09c127b5f4e4bf6f7140ff7ee81046e6301
themeg2 <themeg2@nate.com>
2024-03-15 16:21:02 +0900

    database_update_add_UID

--- database.py
+++ database.py
@@ -7,35 +7,37 @@ def insert_data():
     product = input("제품 이름: ")
     if product == "9999":
         update_data()
-        return  # update_data 호출 후 insert_data 함수로 돌아오도록 함
-    elif product == "8888":  # 삭제 기능 호출 조건 추가
-        delete_data()  # 삭제 함수 호출
+        return
+    elif product == "8888":
+        delete_data()
         return
     else:
         quantity = int(input("수량: "))
         location = input("위치: ")
-        cursor.execute("INSERT INTO inventory (product, quantity, location) VALUES (?, ?, ?)",
-                       (product, quantity, location))
+        uid = input("UID: ")  # 사용자로부터 UID 입력 받음
+        cursor.execute("INSERT INTO inventory (product, quantity, location, uid) VALUES (?, ?, ?, ?)",
+                       (product, quantity, location, uid))
         conn.commit()
 
 def print_inventory():
     cursor.execute("SELECT rowid, * FROM inventory")
     records = cursor.fetchall()
     for row in records:
-        print(f"ID: {row[0]}, 제품 이름: {row[1]}, 수량: {row[2]}, 위치: {row[3]}")
+        print(f"ID: {row[0]}, 제품 이름: {row[1]}, 수량: {row[2]}, 위치: {row[3]}, UID: {row[4]}")  # UID 출력 추가
 
 def update_data():
     print_inventory()
     record_id_input = input("수정할 항목의 ID를 입력하세요: ")
-    if not record_id_input:  # 아무것도 입력하지 않은 경우
-        return  # 바로 함수 종료하고 insert_data로 돌아감
+    if not record_id_input:
+        return
     record_id = int(record_id_input)
     new_product = input("새 제품 이름: ")
     new_quantity = int(input("새 수량: "))
     new_location = input("새 위치: ")
+    new_uid = input("새 UID: ")  # 사용자로부터 새 UID 입력 받음
 
-    cursor.execute("UPDATE inventory SET product = ?, quantity = ?, location = ? WHERE rowid = ?",
-                   (new_product, new_quantity, new_location, record_id))
+    cursor.execute("UPDATE inventory SET product = ?, quantity = ?, location = ?, uid = ? WHERE rowid = ?",
+                   (new_product, new_quantity, new_location, new_uid, record_id))
     conn.commit()
 
 def delete_data():
